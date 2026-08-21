676bdaa162f3af557b3e8cdd93e206e89c011bec
duong24102002 <150357779+duong24102002@users.noreply.github.com>
2026-08-21 09:03:35 +0700

    Create import/export service for holidays data

---
+++ Services/HolidayImportExportService.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using QuanLyNgayLe.Models;
+using Newtonsoft.Json;
+
+namespace QuanLyNgayLe.Services
+{
+    public class HolidayImportExportService
+    {
+        private readonly string _exportPath = Path.Combine(
+            Environment.GetFolderPath(Environment.SpecialFolder.Desktop),
+            "holidays_backup.json"
+        );
+
+        public bool ExportToJson(List<Holiday> holidays)
+        {
+            try
+            {
+                var json = JsonConvert.SerializeObject(holidays, Formatting.Indented);
+                File.WriteAllText(_exportPath, json);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public List<Holiday> ImportFromJson(string filePath)
+        {
+            try
+            {
+                var json = File.ReadAllText(filePath);
+                return JsonConvert.DeserializeObject<List<Holiday>>(json) ?? new List<Holiday>();
+            }
+            catch
+            {
+                return new List<Holiday>();
+            }
+        }
+
+        public string GetExportPath() => _exportPath;
+    }
+}
